04ba732b9a14464e1b4e8c5d899d6eabb81c298f
Gunter Hager <gupi@me.com>
2025-01-26 22:33:49 +0100

    Started to implement loading dropped MZF files.

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-// const fs = std.fs;
-// const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
+const fs = std.fs;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
 const sokol = @import("sokol");
 const sapp = sokol.app;
 const slog = sokol.log;
@@ -15,8 +15,10 @@ const frequencies = mz800.frequencies;
 const name = "MZ-800";
 
 var sys: MZ800 = undefined;
+var gpa = GeneralPurposeAllocator(.{}){};
 
 export fn init() void {
+    std.debug.print("🚨 Booting MZ-800...\n", .{});
     host.audio.init(.{});
     host.time.init();
     host.prof.init();
@@ -25,7 +27,13 @@ export fn init() void {
         .cgrom = @embedFile("system/roms/MZ800_CGROM.bin"),
         .rom2 = @embedFile("system/roms/MZ800_ROM2.bin"),
     } });
-    host.gfx.init(.{ .display = sys.displayInfo() });
+    host.gfx.init(.{
+        .display = sys.displayInfo(),
+        .pixel_aspect = .{
+            .width = 1,
+            .height = 2,
+        },
+    });
 }
 
 export fn frame() void {
@@ -52,8 +60,47 @@ export fn cleanup() void {
 }
 
 export fn input(ev: ?*const sapp.Event) void {
-    _ = ev;
-    // TODO: Implement input events
+    const event = ev.?;
+    // const shift = (0 != (event.modifiers & sapp.modifier_shift));
+
+    switch (event.type) {
+        .CHAR => {
+            const c: u8 = @truncate(event.char_code);
+            std.debug.print("🚨 Char code: {}", .{c});
+        },
+        .KEY_DOWN, .KEY_UP => {
+            const code = event.key_code;
+            std.debug.print("🚨 Key code: {}", .{code});
+        },
+        .FILES_DROPPED => {
+            handleDroppedFiles();
+        },
+        else => {},
+    }
+}
+
+fn handleDroppedFiles() void {
+    const num_of_files: usize = @intCast(sapp.getNumDroppedFiles());
+    std.debug.print("🚨 Files dropped: {}\n", .{num_of_files});
+    for (0..num_of_files) |index| {
+        const path = sapp.getDroppedFilePath(@intCast(index));
+        std.debug.print("🚨 File dropped: {s}\n", .{path});
+        const data = fileLoad(path) catch |err| {
+            std.debug.print("{?}", .{err});
+            return;
+        };
+        std.debug.print("🚨 Read {} bytes.\n", .{data.?.len});
+    }
+}
+
+fn fileLoad(path: []const u8) !?[]const u8 {
+    const allocator = gpa.allocator();
+    var file_data: ?[]const u8 = null;
+    file_data = fs.cwd().readFileAlloc(allocator, path, 0x10000) catch |err| {
+        std.debug.print("Error loading file '{s}'", .{path});
+        return err;
+    };
+    return file_data;
 }
 
 pub fn main() void {
@@ -61,8 +108,10 @@ pub fn main() void {
     const border = host.gfx.DEFAULT_BORDER;
     const width = 2 * display.view.width + border.left + border.right;
     const height = 2 * display.view.height + border.top + border.bottom;
+    std.debug.print("🚨 Display: {}x{}\n", .{ width, height });
     sapp.run(.{
         .init_cb = init,
+        .event_cb = input,
         .frame_cb = frame,
         .cleanup_cb = cleanup,
         .window_title = name,
@@ -70,5 +119,6 @@ pub fn main() void {
         .height = height,
         .icon = .{ .sokol_default = true },
         .logger = .{ .func = slog.func },
+        .enable_dragndrop = true,
     });
 }
