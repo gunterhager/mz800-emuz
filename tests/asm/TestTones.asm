6b6d431650dd52393e7e3a57dbe291333d072b4d
Gunter Hager <gupi@me.com>
2025-05-05 16:32:08 +0200

    Improved sound test program to test all 4 channels

--- tests/asm/TestTones.asm
+++ tests/asm/TestTones.asm
@@ -15,23 +15,62 @@ org 02000h
 
 ; -----------------
 ; Set divider for channel 0 to 0x123
-  ld a, 83h
+  ld a, PSGLatch | PSGChannel0 | 03h
   call set_sound_data
   ld a, 12h
+  call set_sound_data
+
+; Set divider for channel 1 to 0x456
+  ld a, PSGLatch | PSGChannel1 | 04h
+  call set_sound_data
+  ld a, 56h
+  call set_sound_data
+
+; Set divider for channel 2 to 0x789
+  ld a, PSGLatch | PSGChannel2 | 07h
+  call set_sound_data
+  ld a, 89h
   call set_sound_data
 
 ; -----------------
 ; Main loop
 main:
 ; Set attenuation for channel 0 to 0
-  ld a, 90h
+  ld a, PSGLatch | PSGChannel0 | PSGAttenuation
+  call set_sound_data
+  ld a, ColorRed
+  call set_border
+  call wait
+
+; Set attenuation for channel 1 to 0
+  ld a, PSGLatch | PSGChannel1 | PSGAttenuation
+  call set_sound_data
+  ld a, ColorLightRed
+  call set_border
+  call wait
+
+; Set attenuation for channel 1 to 0
+  ld a, PSGLatch | PSGChannel2 | PSGAttenuation
   call set_sound_data
   ld a, ColorYellow
   call set_border
   call wait
 
-; Set attenuation for channel 0 to OFF
-  ld a, 9fh
+; Set attenuation for channel 3 to 0
+  ld a, PSGLatch | PSGChannel3 | PSGAttenuation
+  call set_sound_data
+  ld a, ColorLightYellow
+  call set_border
+  call wait
+
+; Set attenuation for all channels to OFF
+  ld a, PSGLatch | PSGChannel0 | PSGAttenuation | 0fh
+  call set_sound_data
+  ld a, PSGLatch | PSGChannel1 | PSGAttenuation | 0fh
+  call set_sound_data
+  ld a, PSGLatch | PSGChannel2 | PSGAttenuation | 0fh
+  call set_sound_data
+  ld a, PSGLatch | PSGChannel3 | PSGAttenuation | 0fh
   call set_sound_data
   ld a, ColorBlack
   call set_border
